f1ff74276fb69c9016aaa543972b6cf6e4e3be0c
Isuru Hettiarachchi <113895873+IsuruHet@users.noreply.github.com>
2026-03-28 13:07:34 +0530

    script permission change

--- start-keycloak.sh
+++ start-keycloak.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
-# Use Render's PORT or default to 8080
-PORT=${PORT:-8080}
+# Use Render's PORT or default 8080
+PORT=${PORT:-10000}
 
 echo "Starting Keycloak on port $PORT"
 
-# Start Keycloak in dev mode with proper host and port
+# Start Keycloak in dev mode
 /opt/keycloak/bin/kc.sh start-dev --http-host 0.0.0.0 --http-port "$PORT"
